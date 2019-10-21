f694ce3a4830950cc7693ab18ad71b584aa0d79f
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2019-10-21 23:30:25 +0700

    Update carousel and dropdown

--- public/js/mainConfig.js
+++ public/js/mainConfig.js
@@ -1,14 +1,54 @@
 function dropdown() {
     $('.nav-item').mouseover(function() {
-        let toggleTarget = $(this).find('.nav-link').data('toggle');
-        $(`#${toggleTarget}`).show();
+        $(this).find('.dropdown-list').show();
     });
     $('.nav-item').mouseout(function() {
-        let toggleTarget = $(this).find('.nav-link').data('toggle');
-        $(`#${toggleTarget}`).hide();
+        $(this).find('.dropdown-list').hide();
     });
+}
+
+function carousel() {
+    let startPos =  $('.top-movie-wrapper').offset().left;
+    let step = 1256;
+    let counter = 0;
+    $('.top-movie-list').find('.prev-btn').on('click', function(e) {
+        e.preventDefault();
+        if(counter === 0) {
+            return false;
+        }
+        else {
+            counter--;
+            $('.top-movie-wrapper').animate({
+                left: `+=${step}`
+            }, 800);
+        }
+    })
+    $('.top-movie-list').find('.next-btn').on('click', function(e) {
+        e.preventDefault();
+        if(counter === 3) {
+            counter=0;
+            $('.top-movie-wrapper').animate({
+                left: 12
+            }, 1000);
+            return false;
+        }
+        else {
+            counter++;
+            $('.top-movie-wrapper').animate({
+                left: `-=${step}`
+            }, 800);
+        } 
+    })
+};
+
+function autoCarousel() {
+    $('.top-movie-list').find('.next-btn').click();
 }
 
 $(document).ready(function () {
     dropdown();
+    carousel();
+    setInterval(function() {
+        autoCarousel();
+    }, 5000);
 });
